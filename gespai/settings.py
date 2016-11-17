1d1207c70fcb3d6e9000ff735d969dbf8a1dd883
Sergio Rodríguez <sjrodmar@gmail.com>
2016-11-17 13:39:00 +0000

    Creada aplicación de Django para el sistema de carga de datos

--- gespai/settings.py
+++ gespai/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gestion.apps.GestionConfig',
+    'upload.apps.UploadConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
