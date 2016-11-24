cceab2272b2a41141017f49fb6ae46abbbf8cd80
Sergio Rodríguez <sjrodmar@gmail.com>
2016-11-24 10:36:07 +0000

    Creada rama para la conexión a la BD MySQL de la máquina en IaaS

--- gespai/settings.py
+++ gespai/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'gespai.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+		'read_default_file': '/home/usuario/gespai.cnf',
+		'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+	},
     }
 }
 
