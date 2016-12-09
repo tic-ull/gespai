179e3d482cab6115f7cc6864a098884eba11ad1b
Sergio Rodríguez <sjrodmar@gmail.com>
2016-12-09 15:18:24 +0000

    Finalizada implementación del formulario para crear cambios pendientes, closes #28

--- cambios/forms.py
+++ cambios/forms.py
@@ -4,13 +4,12 @@ from gestion import models
 
 
 class CambioBecarioForm(forms.Form):
-    plazas = models.Plaza.objects.all()
-    plazas_libres_pks = []
-    for plaza in plazas:
-        if not plaza.becario_set.all():
-            plazas_libres_pks.append(plaza.pk)
-
-    plazas_libres = models.Plaza.objects.filter(pk__in=plazas_libres_pks)
+    def __init__(self, *args, **kwargs):
+        self.becario = kwargs.pop('becario')
+        super(CambioBecarioForm, self).__init__(*args, **kwargs)
+        print(unicode(self.becario.email))
+        if self.becario.plaza_asignada:
+            self.fields['plaza_cambio'].initial = self.becario.plaza_asignada
 
     ESTADOS = (
         ('A', 'Asignado'),
@@ -18,11 +17,18 @@ class CambioBecarioForm(forms.Form):
         ('T', 'Traslado'),
     )
 
-    plaza_cambio = forms.ModelChoiceField(queryset=plazas_libres)
+    plaza_cambio = forms.ModelChoiceField(queryset=models.Plaza.objects.all())
     # TODO: Si es estado_cambio = 'Traslado', plaza_cambio no debe ser null
     estado_cambio = forms.ChoiceField(choices=ESTADOS)
-    formatos_fecha = ['%d-%m-%Y',
-                      '%d-%m-%y',
-                      '%d/%m/%Y',
-                      '%d/%m/%y']
-    fecha_cambio = forms.DateField(input_formats=formatos_fecha, widget=forms.SelectDateWidget)
+    fecha_cambio = forms.DateField(widget=forms.SelectDateWidget)
+
+    #def clean(self):
+
+def get_plazas_libres():
+    plazas = models.Plaza.objects.all()
+    plazas_libres_pks = []
+    for plaza in plazas:
+        if not plaza.becario_set.all():
+            plazas_libres_pks.append(plaza.pk)
+
+    return models.Plaza.objects.filter(pk__in=plazas_libres_pks)
--- cambios/views.py
+++ cambios/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from django.core.exceptions import ValidationError
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 from gestion import models
 from . import forms
@@ -17,9 +20,20 @@ def cambio_becario(request, orden_becario):
         becario = None
 
     if request.method == 'POST':
-        form = forms.CambioBecarioForm(request.POST)
+        form = forms.CambioBecarioForm(request.POST, becario=becario)
         if form.is_valid():
-            print('magia')
+            print(form.cleaned_data)
+            new_cambio_pendiente = models.CambiosPendientes(becario=becario,
+            plaza=form.cleaned_data['plaza_cambio'],fecha_cambio=form.cleaned_data['fecha_cambio'],
+            estado_cambio=form.cleaned_data['estado_cambio'])
+            try:
+                new_cambio_pendiente.full_clean()
+                new_cambio_pendiente.save()
+            except ValidationError as e:
+                messages.error(request, e.message)
+                return HttpResponseRedirect('/cambios/' + orden_becario)
+            return HttpResponseRedirect('/cambios')
+
     else:
-        form = forms.CambioBecarioForm()
+        form = forms.CambioBecarioForm(becario=becario)
     return render(request, 'cambios/cambio_becario.html', {'becario': becario, 'form': form})
