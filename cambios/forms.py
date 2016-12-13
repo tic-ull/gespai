84913ab284c2441764206d1d731a03b4f5db7593
Sergio Rodríguez <sjrodmar@gmail.com>
2016-12-13 09:21:32 +0000

    Ahora se muestra un mensaje cuando se solicita un cambio con éxito #28, cambiados algunos HttpResponseRedirect #32

--- cambios/forms.py
+++ cambios/forms.py
@@ -7,8 +7,7 @@ class CambioBecarioForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.becario = kwargs.pop('becario')
         super(CambioBecarioForm, self).__init__(*args, **kwargs)
-        print(unicode(self.becario.email))
-        if self.becario.plaza_asignada:
+        if self.becario and self.becario.plaza_asignada:
             self.fields['plaza_cambio'].initial = self.becario.plaza_asignada
 
     ESTADOS = (
@@ -18,17 +17,5 @@ class CambioBecarioForm(forms.Form):
     )
 
     plaza_cambio = forms.ModelChoiceField(label="Plaza de cambio", queryset=models.Plaza.objects.all())
-    # TODO: Si es estado_cambio = 'Traslado', plaza_cambio no debe ser null
     estado_cambio = forms.ChoiceField(label="Estado de cambio", choices=ESTADOS)
     fecha_cambio = forms.DateField(label="Fecha de cambio", widget=forms.SelectDateWidget)
-
-    #def clean(self):
-
-def get_plazas_libres():
-    plazas = models.Plaza.objects.all()
-    plazas_libres_pks = []
-    for plaza in plazas:
-        if not plaza.becario_set.all():
-            plazas_libres_pks.append(plaza.pk)
-
-    return models.Plaza.objects.filter(pk__in=plazas_libres_pks)
--- cambios/views.py
+++ cambios/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.core.exceptions import ValidationError
 from django.contrib import messages
-from django.http import HttpResponseRedirect
-import pdb
+from django.shortcuts import redirect
 
 from gestion import models
 from . import forms
@@ -24,7 +23,6 @@ def cambio_becario(request, orden_becario):
     if request.method == 'POST':
         form = forms.CambioBecarioForm(request.POST, becario=becario)
         if form.is_valid():
-            print("El form esta bien")
             new_cambio_pendiente = models.CambiosPendientes(becario=becario,
             plaza=form.cleaned_data['plaza_cambio'],fecha_cambio=form.cleaned_data['fecha_cambio'],
             estado_cambio=form.cleaned_data['estado_cambio'])
@@ -34,11 +32,12 @@ def cambio_becario(request, orden_becario):
             except ValidationError as e:
                 if e.messages[0].startswith('Cambios pendientes'):
                     messages.error(request, "Ya existe una solicitud de cambio para este becario y esta plaza\
-                    para la fecha indicada.")
+                    para la fecha indicada.", extra_tags='alert alert-danger')
                 else:
-                    messages.error(request, e.messages[0])
-                return HttpResponseRedirect('/cambios/' + orden_becario)
-            return HttpResponseRedirect('/cambios')
+                    messages.error(request, e.messages[0], extra_tags='alert alert-danger')
+                return redirect('cambios:cambio', orden_becario=orden_becario)
+            messages.success(request, "Cambio solicitado con éxito", extra_tags='alert alert-success')
+            return redirect('cambios:cambio', orden_becario=orden_becario)
 
     else:
         form = forms.CambioBecarioForm(becario=becario)
