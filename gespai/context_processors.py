8874d0288617b57ce0813bbd8a52d33d7a01e90d
Sergio Rodríguez <sjrodmar@gmail.com>
2016-12-13 13:52:49 +0000

    Ahora se muestran las notificaciones no leídas en la página web
    * Creado método context_processor para pasar variables a las plantillas
    * Las notificaciones se muestran solo a miembros del grupo osl

---
+++ gespai/context_processors.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+
+# Método que devuelve un diccionario con información que se utilizará
+# en las plantillas de la aplicación
+def context_processor(request):
+    return {'is_osl': request.user.groups.filter(name='osl').exists()}
