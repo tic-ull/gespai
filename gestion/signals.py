25429caec86ecbc883dfcb6048ef91ba1d7b5f68
Sergio Rodríguez <sjrodmar@gmail.com>
2016-12-19 09:53:36 +0000

    Cambios en notificaciones al editar cambios #36
    Cuando se edita un cambio, se borra la notificación existente para dicho cambio y se genera una nueva con los nuevos datos del cambio

--- gestion/signals.py
+++ gestion/signals.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from gestion.models import CambiosPendientes
@@ -10,6 +11,14 @@ from notifications.models import Notification
 def cambio_estado(sender, **kwargs):
     osl = Group.objects.get(name='osl')
     instance = kwargs.get('instance')
+    # En caso de que ya haya una notificación para el cambio que se va a guardar
+    # (si se modifica un cambio ya existente) se borra la notificación y se genera una nueva
+    try:
+        notificacion = Notification.objects.get(
+            action_object_object_id=instance.pk)
+        notificacion.delete()
+    except Notification.DoesNotExist:
+        pass
     if instance.plaza:
         desc = 'Tipo de cambio: ' + instance.get_estado_cambio_display() + '\nPlaza asociada al cambio: ' + \
             unicode(instance.plaza) + '\nFecha del cambio: ' + \
@@ -20,11 +29,14 @@ def cambio_estado(sender, **kwargs):
     notify.send(instance.becario, recipient=osl, verb='tiene un nuevo cambio solicitado',
                 action_object=instance, target=instance.becario, description=desc)
 
+
+# Si se borra un cambio, se borra la notificación asociada al mismo
 @receiver(post_delete, sender=CambiosPendientes)
 def borrar_notificacion(sender, **kwargs):
     instance = kwargs.get('instance')
     try:
-        notificacion = Notification.objects.get(action_object_object_id=instance.pk)
+        notificacion = Notification.objects.get(
+            action_object_object_id=instance.pk)
         notificacion.delete()
     except Notification.DoesNotExist:
         pass
