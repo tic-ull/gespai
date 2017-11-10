912c92aae8b398b34fd9b081a12fd80fb1055a7c
jeplasenciap <jeplasenciap@gmail.com>
2017-11-10 13:18:42 +0000

    (#1) arreglar problema con validación de NIEs

--- gespai/validation.py
+++ gespai/validation.py
@@ -28,12 +28,12 @@ def is_dni(dni):
 
 _NIE_PATRON_REGEX = r"^[XYZ]\d{7}[A-Z]$"
 _NIE_LETRA_SUBSTITUCION = {"X": "0", "Y": "1", "Z": "2"}
-_NIE_REGEX = re.compile(_DNI_PATRON_REGEX)
+_NIE_REGEX = re.compile(_NIE_PATRON_REGEX)
 def is_nie(nie):
     try:
         nie_sub = _NIE_LETRA_SUBSTITUCION[nie[0]] + nie[1:-1]
         letra_verificacion = _DNI_VERIFICACION[int(nie_sub) % _DNI_MODULO_VERIFICACION]
-    except:
+    except Exception:
         return False
     return _NIE_REGEX.match(nie) and letra_verificacion == nie[-1]
 
